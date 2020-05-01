0d232e67a8458ab44546696ba8ebf84c0ab41e29
Ranjani <ranani26063@gmail.com>
2020-05-01 14:33:51 +0200

    trial alice

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
-    public class Hello {
-        public static void greet(String name) {
-            System.out.println(greeting() +
-                    ", " + name + "!");
+public class Hello {
+    public static void greet(String name) {
+        System.out.println(greeting() +
+                ", " + name + "!");
 
-        }
-        public static String greeting() {
-            return "Hello";
-        }
     }
+    public static String greeting() {
+        return "Hello";
+    }
+}

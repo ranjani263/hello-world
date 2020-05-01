4e471fcd1044f207242c0e42be816c84da77f09c
Ranjani <ranani26063@gmail.com>
2020-05-01 13:59:56 +0200

    trial master

---
+++ Hello.java
@@ -0,0 +1,8 @@
+    public class Hello {
+        public static void greet(String name) {
+            System.out.println(greeting() + ", " + name);
+        }
+        public static String greeting() {
+            return "Hello";
+        }
+    }
